f1598a5360b51bad0d657e170746deb45b3675c6
laujonat <jon@jonathanlau.io>
2021-07-26 01:33:44 -0700

    update

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Setup
 
+## Usage
+
+Assumes you have VSCode installed.
+
+Fill in the fields in `paste-this-into-vscode-user-settings.json` and copy and paste them into your VSCode user settings.
+
+```json
+  "psi-header.variables": [
+    ["author", ""],
+    ["authoremail", ""],
+    ["initials", ""],
+    ["github-user", ""]
+  ]
+```
+
+For convenience, you can also run this somewhat useless script.
 
 ```bash
 $ chmod +rx ext-setup.sh
 $ ./ext-setup.sh
 ```
+
+This should output the configuration settings with errors. Copy the block of valid JSON and paste into user settings.
--- ext-setup.sh
+++ ext-setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
 # Check for VSCode
 if ! [ -x "$(command -v code)" ]; then
   echo 'Error: VSCode is not installed.' >&2
   exit 1
 else
-  # code --install-extension psioniq.psi-header
   echo "vscode found. OK"
 fi
 
@@ -20,13 +18,135 @@ if [ "" = "$PKG_OK" ]; then
   echo "No $REQUIRED_PKG found. Setting up $REQUIRED_PKG."
 fi
 
-
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  code --install-extension psioniq.psi-header
   sudo apt-get --yes install $REQUIRED_PKG
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+  code --install-extension psioniq.psi-header
   brew install $REQUIRED_PKG
   exit 0
 else
   echo "Unsupported OS. Sorry."
   exit 0
 fi
+
+
+# fullname="USER INPUT"
+read -p "Enter fullname: " fullname
+# user="USER INPUT"
+read -p "Enter email: " email
+# fullname="USER INPUT"
+read -p "Enter initials: " initials
+# user="USER INPUT"
+read -p "Enter github username: " username
+
+echo jq --arg fullname "$fullname" --arg email "$email" --arg \ username "$username" --arg initials "$initials" '.fullname, .email, .username, .initials' '
+  {
+    "psi-header.variables": [
+      ["company", "Hakkei Co."],
+      ["author", "'$fullname'"],
+      ["authoremail", "'$email'"],
+      ["website", "https://hakkei.co"],
+      ["initials", "'$initials'"],
+      ["projectCreationYear", "2021"],
+      ["github-org", "github/hakkei-co"],
+      ["github-user", "'$username'"]
+    ],
+    "psi-header.config": {
+      "forceToTop": true,
+      "blankLinesAfter": 4
+    },
+    "psi-header.changes-tracking": {
+      "autoHeader": "autoSave",
+      "isActive": true,
+      "modAuthor": "@lastModifiedBy:   ",
+      "modDate": "@lastModifiedTime: ",
+      "include": [],
+      "includeGlob": [],
+      "exclude": ["markdown", "yaml", "yml", "json", "jsonc", "shellscript"],
+      "excludeGlob": ["./**/*/ignoreme.*"],
+      "enforceHeader": true,
+      "replace": ["Filename:", "Project"],
+      "updateLicenseVariables": false
+    },
+    "psi-header.lang-config": [
+      {
+        "language": "javascript",
+        "begin": "/*",
+        "prefix": "",
+        "suffix": " ",
+        "lineLength": 80,
+        "end": " */",
+        "forceToTop": true,
+        "blankLinesAfter": 3,
+        "beforeHeader": [],
+        "afterHeader": [],
+        "rootDirFileName": "package.json",
+        "modAuthor": "Modified By:",
+        "modDate": "Last Modified:",
+        "modDateFormat": "dd/MM/yyyy hh:nn:ss",
+        "replace": ["Filename:", "Project"],
+        "ignoreLines": []
+      },
+      {
+        "language": "typescript",
+        "mapTo": "javascript"
+      },
+      {
+        "language": "markdown",
+        "begin": "\n",
+        "prefix": "[//]: <> ",
+        "suffix": "",
+        "end": " ",
+        "forceToTop": true,
+        "blankLinesAfter": 2
+      }
+    ],
+    "psi-header.templates": [
+      {
+        "language": "*",
+        "template": [
+          "Copyright (c) <<year>> <<company>> <<github-org>>",
+          "",
+          "Created Date: <<filecreated('\'dddd, MMMM Do YYYY, h:mm:ss a\'')>>",
+          "Author: <<author>> <<github-user>>",
+          "",
+          "Redistribution and use in source and binary forms, with or without",
+          "modification, are permitted provided that the following conditions are",
+          "met:",
+          "",
+          "1. Redistributions of source code must retain the above copyright notice,",
+          "this list of conditions and the following disclaimer.",
+          "",
+          "2. Redistributions in binary form must reproduce the above copyright",
+          "notice, this list of conditions and the following disclaimer in the",
+          "documentation and/or other materials provided with the distribution.",
+          "",
+          "3. Neither the name of the copyright holder nor the names of its",
+          "contributors may be used to endorse or promote products derived from this",
+          "software without specific prior written permission.",
+          "",
+          "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS",
+          "IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,",
+          "THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR ",
+          "PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR",
+          "CONTRIBUTORS BE",
+          "LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR",
+          "CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF",
+          "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS",
+          "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN",
+          "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)",
+          "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF",
+          "THE POSSIBILITY OF SUCH DAMAGE.",
+          "",
+          "HISTORY:",
+          "Date      \tBy\tComments",
+          "----------\t---\t----------------------------------------------------------"
+        ],
+        "changeLogCaption": "HISTORY:",
+        "changeLogHeaderLineCount": 2,
+        "changeLogEntryTemplate": ["<<dateformat('YYYY-MM-DD')>>\t<<initials>>\t"]
+      }
+    ]
+  }
+' > psioniq-header-settings.json
